95c623287e31ade92b0e6f14fda10f0141647e7b
SYED-YUNUS-AHMED <syedyunus1307@gmail.com>
2026-09-08 11:35:15 +0530

    Add files via upload

---
+++ 005_Divisibility_by_3_5_both_on_either.py
@@ -0,0 +1,10 @@
+# 5. Divisibility by 3, 5, both, or neither
+n = int(input("Enter a number: "))
+if n % 3 == 0 and n % 5 == 0:
+    print("Divisible by both 3 and 5")
+elif n % 3 == 0:
+    print("Divisible by 3")
+elif n % 5 == 0:
+    print("Divisible by 5")
+else:
+    print("Not divisible by 3 or 5")
