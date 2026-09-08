5d8aad5f83d5ecba6e32d260193ee43583330c7d
SYED-YUNUS-AHMED <syedyunus1307@gmail.com>
2026-09-08 14:25:04 +0530

    Add files via upload

---
+++ 035_Alphabet_digit_or_special_character.py
@@ -0,0 +1,9 @@
+# 35. Alphabet, digit, or special character
+ch = input("Enter a character: ")
+if ch.isalpha():
+    print("Alphabet")
+elif ch.isdigit():
+    print("Digit")
+else:
+    print("Special character")
+    
