6b44d351561ce540d9a9ffa6911ef0051334d86b
SYED-YUNUS-AHMED <syedyunus1307@gmail.com>
2026-09-08 11:32:15 +0530

    Add files via upload

---
+++ 004_Celsius_to_Fahrenheit_vice_versa.py
@@ -0,0 +1,8 @@
+# 4. Celsius <-> Fahrenheit
+c = float(input("Enter temperature in Celsius: "))
+f = (c * 9/5) + 32
+print("Fahrenheit:", f)
+
+f2 = float(input("Enter temperature in Fahrenheit: "))
+c2 = (f2 - 32) * 5/9
+print("Celsius:", c2)
