a4280c806ce3b98781d3ef49e92321928e714ba0
SYED-YUNUS-AHMED <syedyunus1307@gmail.com>
2026-09-08 14:19:49 +0530

    Add files via upload

---
+++ 032_Positive_negative_zero_then_even_odd_if_positive.py
@@ -0,0 +1,12 @@
+# 32. Positive/negative/zero, then even/odd if positive
+n = int(input("Enter a number: "))
+if n > 0:
+    if n % 2 == 0:
+        print("Positive and Even")
+    else:
+        print("Positive and Odd")
+elif n < 0:
+    print("Negative")
+else:
+    print("Zero")
+    
