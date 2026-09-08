c09422ae5009cfc176c76e29376924db6ef43571
SYED-YUNUS-AHMED <syedyunus1307@gmail.com>
2026-09-08 14:57:00 +0530

    Add files via upload

---
+++ 041_Clock_angle_between_hour_and_minute_hands.py
@@ -0,0 +1,8 @@
+# 41. Clock angle between hour and minute hands
+hour = int(input("Enter hour (1-12): "))
+minute = int(input("Enter minute (0-59): "))
+hour_angle = (hour % 12) * 30 + minute * 0.5
+minute_angle = minute * 6
+angle = abs(hour_angle - minute_angle)
+angle = min(angle, 360 - angle)
+print("Angle between hands:", angle)
