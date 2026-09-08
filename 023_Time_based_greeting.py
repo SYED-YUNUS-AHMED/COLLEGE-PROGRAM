4fd4eca9bcb4a056915647fb02fcd707b13e2d77
SYED-YUNUS-AHMED <syedyunus1307@gmail.com>
2026-09-08 12:07:53 +0530

    Add files via upload

---
+++ 023_Time_based_greeting.py
@@ -0,0 +1,10 @@
+# 23. Time-based greeting
+hour = int(input("Enter hour (0-23): "))
+if hour < 12:
+    print("Good Morning")
+elif hour < 17:
+    print("Good Afternoon")
+elif hour < 21:
+    print("Good Evening")
+else:
+    print("Good Night")
