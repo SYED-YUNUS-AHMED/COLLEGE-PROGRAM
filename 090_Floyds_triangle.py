37b506e7b941a1e047a0b4f94408b6e102d10d7f
SYED-YUNUS-AHMED <syedyunus1307@gmail.com>
2026-09-09 12:11:50 +0530

    Add files via upload

---
+++ 090_Floyds_triangle.py
@@ -0,0 +1,9 @@
+# 90. Floyd's triangle
+n = int(input("Enter number of rows: "))
+num = 1
+for i in range(1, n + 1):
+    row = ""
+    for j in range(i):
+        row += str(num) + " "
+        num += 1
+    print(row)
